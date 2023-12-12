544e0584bf92b7a269eaf0b2a15b84be2ce0b4f1
lewismcfarlane <lewismcf@ymail.com>
2023-12-12 19:06:13 +0000

    fix: Correct amount of divs in container

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // On load: create 16 divs which will form the etch-a-sketch
 window.addEventListener('load', event => {
     let mainGridContainer = document.querySelector('#mainGridContainer')
-    for (let i = 1; i<=16; i++) {
+    for (let i = 1; i<=256; i++) {
         let gridDiv = document.createElement(`div`);
         gridDiv.classList.add("gridItem");
         gridDiv.id = `grid${i}`;
