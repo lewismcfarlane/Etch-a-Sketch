add4d0b0c23f9570b8b5034a407680f9d43b3dfb
lewismcfarlane <lewismcf@ymail.com>
2023-12-12 20:01:28 +0000

    feat: Add etch-a-sketch functionality

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
-// On load: create 16 divs which will form the etch-a-sketch
+let gridContainer = document.querySelector('#gridContainer')
+
+// On load: create 256 (16x16) divs which will form the etch-a-sketch
 window.addEventListener('load', event => {
-    let mainGridContainer = document.querySelector('#mainGridContainer')
+    
     for (let i = 1; i<=256; i++) {
         let gridDiv = document.createElement(`div`);
         gridDiv.classList.add("gridItem");
         gridDiv.id = `grid${i}`;
-        mainGridContainer.appendChild(gridDiv);
+        gridContainer.appendChild(gridDiv);
+        // Attach mousedown event to each grid item
+        gridDiv.addEventListener('mousedown', handleDrag);
     }
 })
+
+let isMouseDown = false;
+
+let handleDrag = event => {
+    // Mouse button is down
+    isMouseDown = true;
+    // Draws on the div grid square
+    if (event.target.classList.contains('gridItem')) {
+        event.target.classList.add('drawOnGrid');
+    }
+    // Add mouseup and mousemove event listeners
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+};
+
+// Function to draw on grid if mouse is moving from div to div
+let handleMouseMove = (event) => {
+    // Draws on grid if entering a new grid
+    if (isMouseDown && event.target.classList.contains('gridItem')) {
+        event.target.classList.add('drawOnGrid');
+    }
+};
+
+// function to respond to left mouse button not being pressed
+let handleMouseUp = () => {
+    isMouseDown = false;
+    // Removes event listeners after mouse button is up
+    document.removeEventListener('mousemove', handleMouseMove);
+    document.removeEventListener('mouseup', handleMouseUp);
+}
