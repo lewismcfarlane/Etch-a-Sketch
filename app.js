368a89648d386806f9f702812326fe7f284a4606
lewismcfarlane <lewismcf@ymail.com>
2023-12-12 19:00:43 +0000

    chore: Assign class and ID to divs

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// On load: create 16 divs which will form the etch-a-sketch
 window.addEventListener('load', event => {
     let mainGridContainer = document.querySelector('#mainGridContainer')
     for (let i = 1; i<=16; i++) {
         let gridDiv = document.createElement(`div`);
-        gridDiv.id = `gridDiv${i}`;
+        gridDiv.classList.add("gridItem");
+        gridDiv.id = `grid${i}`;
         mainGridContainer.appendChild(gridDiv);
     }
 })
